a883e55485f53c9f11d81b75a18cb7969e816f60
unknown <guillermoacdc@hotmail.com>
2023-05-09 16:05:10 -0500

    generalize for the name of estimated poses

--- incrementalApproachPlanes/Main_secondStage.m
+++ incrementalApproachPlanes/Main_secondStage.m
@@ -10,8 +10,8 @@ randomV=[1 2 1 1 2 2 1 2 2 1 2 2 1 1 1 2 1 2];
 sessionsID=[3	10	12	13	17	19	20	25	27	32	33	45	52	53	54];%35	36	39
 Ns=size(sessionsID,2);
 planeType=1;
-fileName1='estimatedPoses_ia1_t1.json';
-fileName2='estimatedPoses_ia2_t1.json';
+fileName1=['estimatedPoses_ia1_t' num2str(planeType) '.json'];
+fileName2=['estimatedPoses_ia2_t' num2str(planeType) '.json'];
 for i=1:Ns
     sessionID=sessionsID(i);
     if randomV(i)==1
